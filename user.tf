10ab02b3c7ef4d3ffa75e7d3727b61c569ca3438
Stephen Stonehouse <stephen.stonehouse@claranet.uk>
2018-06-28 12:03:32 +0100

    Control read/write access per repository.

--- user.tf
+++ user.tf
@@ -3,30 +3,6 @@ resource "aws_iam_user" "user" {
   path = "/"
 }
 
-resource "aws_iam_user_policy_attachment" "codecommitrw-attach" {
-  count = "${var.read_only ? 0 : 1}"
-
-  user       = "${aws_iam_user.user.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitFullAccess"
-}
-
-resource "aws_iam_user_policy_attachment" "codecommitro-attach" {
-  count = "${var.read_only ? 1 : 0}"
-
-  user       = "${aws_iam_user.user.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitReadOnly"
-}
-
-resource "aws_iam_user_policy_attachment" "iamssh-attach" {
-  user       = "${aws_iam_user.user.name}"
-  policy_arn = "arn:aws:iam::aws:policy/IAMUserSSHKeys"
-}
-
-resource "aws_iam_user_policy_attachment" "iamro-attach" {
-  user       = "${aws_iam_user.user.name}"
-  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
-}
-
 resource "aws_iam_user_ssh_key" "user" {
   username   = "${aws_iam_user.user.name}"
   encoding   = "PEM"
