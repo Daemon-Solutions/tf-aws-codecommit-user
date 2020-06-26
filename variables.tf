6f9ba15581881f9626b94816634c2ea98ff5efd5
Callum Fox <callum.fox@claranet.uk>
2020-06-26 15:24:07 +0100

    Fix type definitions for TF12.

--- variables.tf
+++ variables.tf
@@ -1,26 +1,27 @@
 variable "user_name" {
   description = "The username for the user to create"
-  type        = "string"
+  type        = string
 }
 
 variable "user_key" {
   description = "The key for the user to create"
-  type        = "string"
+  type        = string
 }
 
 variable "write_repos" {
   description = "ARNs of the repositories to grant write access"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "read_repos" {
   description = "ARNs of the repositories to grant read access"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "random_byte_length" {
   description = "The byte length of the random id generator used for unique resource names."
+  type        = number
   default     = 4
 }
